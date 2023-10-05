05fc907d29749965f5508fc201e0ff95b43974fc
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 12:01:13 +0100

    deps

--- deps.ts
+++ deps.ts
@@ -2,9 +2,35 @@
 export { Status } from "$std/http/mod.ts";
 /* Hono */
 export { Hono, HTTPException } from "$hono/mod.ts";
-export type { Handler } from "$hono/mod.ts";
+export type { Handler, MiddlewareHandler } from "$hono/mod.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
 export { html, logger } from "$hono/middleware.ts";
 /* Valibot */
 export { parse, object, optional, string, maxBytes } from "$valibot/mod.ts";
 export type { Output } from "$valibot/mod.ts";
+/* LibSql */
+/* std */
+export { encode } from "$std/encoding/base64.ts";
+/* libsql */
+export {
+    LibsqlError,
+    type Config,
+    type Client,
+    type IntMode,
+    type InStatement,
+    type InArgs,
+    type InValue,
+    type ResultSet,
+    type Row,
+    type Transaction,
+    type TransactionMode,
+    type Value,
+} from "$libsql/api.js";
+export {
+    expandConfig,
+    type ExpandedConfig,
+} from "$libsql/config.js";
+export { _createClient as _createWsClient } from "$libsql/ws";
+export { _createClient as _createHttpClient } from "$libsql/http";
+import Database from "libsql";
+export { Database };
