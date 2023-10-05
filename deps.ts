d1920b4e75e0d3f0cea5cb67ce151c83fec37714
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 13:18:19 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -6,7 +6,7 @@ export type { Handler, MiddlewareHandler } from "$hono/mod.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
 export { html, logger } from "$hono/middleware.ts";
 /* Valibot */
-export { parse, object, optional, string, maxBytes } from "$valibot/mod.ts";
+export { transform,parse, object, optional, string, maxBytes } from "$valibot/mod.ts";
 export type { Output } from "$valibot/mod.ts";
 /* LibSql */
 /* std */
