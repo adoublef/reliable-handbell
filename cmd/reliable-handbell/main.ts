1e83ac385149c2865b6f52e4510c7814a94697ae
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 12:01:02 +0100

    add code for turso client

--- cmd/reliable-handbell/main.ts
+++ cmd/reliable-handbell/main.ts
@@ -1,17 +1,29 @@
+import { env } from "~/lib/env.ts";
 import { Hono, logger } from "~/deps.ts";
-import { handleIndex } from "./handle_index.tsx";
 import { serve } from "~/lib/serve.ts";
+import { handleIndex } from "./handle_index.tsx";
 import { handleAbout } from "./handle_about.tsx";
+import { handleError } from "./handle_error.tsx";
+import { ping } from "~/lib/turso.ts";
+import { createClient } from "~/lib/libsql/mod.ts";
 
 if (
     import.meta.main
 ) {
-    const app = new Hono();
+    const db = createClient({ url: env("DATABASE_URL") });
+    await ping(db);
 
-    app.use("*", logger())    
+    const app = new Hono()
+        .use("*", logger())
+        .onError((_err, c) => c.redirect("/ouch"));
+
     app.get("/", handleIndex());
     app.get("/about", handleAbout());
+    app.get("/ouch", handleError());
 
     await serve(app);
+
+    // TODO -- handle graceful shutdown
+    // required to sync changes to Turso
 }
 
